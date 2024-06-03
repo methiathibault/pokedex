040158266a868bf38dd201aa87d58999482e9bf2
methiathibault <methiathibault@gmail.com>
2024-06-03 23:45:44 +0200

    view all pokemons

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import '../styles/PokemonList.css'
 
 export default function PokemonList() {
     const [pokemons, setPokemons] = useState([])
@@ -33,19 +34,25 @@ export default function PokemonList() {
     }
 
     return (
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridGap: '10px' }}>
+        <div className="pokemon-grid">
             {pokemons.length !== 0 && 
             <>
                 {pokemons.map(pokemon => (
-                    <div key={pokemon.id}>
-                        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                        {pokemon.name} 
+                    <div key={pokemon.id} className='pokemon-card'>
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name} className='pokemon-image' />
+                        <p className='pokemon-name'>{pokemon.name}</p>
+                        <p className='pokemon-id'>{pokemon.id}</p>
+                        <p className='pokemon-type'>
+                        {pokemon.types.map(type => (
+                            <span key={type.slot}>{type.type.name} </span>
+                        ))}
+                        </p>
                     </div>
                 ))}
             </>
             }
-            <button onClick={() => setPage(prevPage => prevPage - 1)} disabled={page === 1}>Previous</button>
-            <button onClick={() => setPage(prevPage => prevPage + 1)}>Next</button>
+            <button className='pagination-button' onClick={() => setPage(prevPage => prevPage - 1)} disabled={page === 1}>Previous</button>
+            <button className='pagination-button' onClick={() => setPage(prevPage => prevPage + 1)}>Next</button>
         </div>
     )
 }
