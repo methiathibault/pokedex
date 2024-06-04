15b9bb6fb4c18ff622b8bda8a592df11a8ff54e7
methiathibault <methiathibault@gmail.com>
2024-06-04 12:18:28 +0200

    finished pokedex

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -2,13 +2,30 @@ import React, {useState, useEffect} from 'react'
 import PokemonCardList from './PokemonCardList'
 
 export default function Pokedex() {
+    const [input, setInput] = useState('');
+    const [searchedPokemon, setSearchedPokemon] = useState(null);
+
     function pokemonJsonToObject(pokemonJson) {
         return JSON.parse(pokemonJson);
     }        
 
+    function getPokemon(pokemon) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            .then(response => response.json())
+            .then(data => setSearchedPokemon(data))
+            .catch(error => console.error(error))
+    }
+
     const pokemons = Object.keys(localStorage).map(key => pokemonJsonToObject(localStorage.getItem(key)));
 
     return (
-        <PokemonCardList pokemons={pokemons} isPokemonList={false} />
+        <div className='pokedex'>
+            <div className='pokemon-search'>
+                <input type='text' onChange={(e) => setInput(e.target.value)} className='pokemon-input'/>
+                <button className='pokemon-button' onClick={() => getPokemon(input)}>Search</button>
+            </div>
+            {searchedPokemon && <PokemonCardList pokemons={[searchedPokemon]} isPokemonList={true} />}
+            <PokemonCardList pokemons={pokemons} isPokemonList={false} />
+        </div>
     )
 }
